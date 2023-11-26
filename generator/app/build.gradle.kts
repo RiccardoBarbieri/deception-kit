9edf9a8662f63552aedc57866cef16f0e1ff16f0
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2023-11-27 00:20:55 +0100

    Dependency management

--- generator/app/build.gradle.kts
+++ generator/app/build.gradle.kts
@@ -7,8 +7,17 @@
 
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
-    application
+    id("java")
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+group = "com.deceptionkit"
+version = "0.0.1"
 
 repositories {
     // Use Maven Central for resolving dependencies.
@@ -24,16 +33,23 @@ dependencies {
 
     // Keycloak
     implementation("org.keycloak:keycloak-admin-client:15.0.2")
+
+	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.session:spring-session-core")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    implementation("org.yaml:snakeyaml:2.2")
+
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
-    }
-}
 
-application {
-    // Define the main class for the application.
-    mainClass.set("deceptionkit.App")
-}
+//application {
+//    // Define the main class for the application.
+//    mainClass.set("deceptionkit.App")
+//}
