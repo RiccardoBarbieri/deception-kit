b1339c468888b1db27cd1de82a361c0242a890aa
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2023-12-02 16:39:43 +0100

    new simplified Group model, completed UserRegistration and GroupRegistration

--- generator/src/main/java/com/deceptionkit/registration/GroupRegistration.java
+++ generator/src/main/java/com/deceptionkit/registration/GroupRegistration.java
@@ -1,33 +1,70 @@
 package com.deceptionkit.registration;
 
+import com.deceptionkit.model.Group;
 import com.deceptionkit.model.User;
+import com.deceptionkit.response.model.SimpleResponse;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.GroupRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class GroupRegistration {
+
+    private final Logger logger;
 
     private final Keycloak keycloak;
 
     @Autowired
     public GroupRegistration(Keycloak keycloak) {
         this.keycloak = keycloak;
+        this.logger = LoggerFactory.getLogger(GroupRegistration.class);
+    }
+
+    private GroupRepresentation generateGroupRep(Group g) {
+        GroupRepresentation group = new GroupRepresentation();
+        group.setName(g.getName());
+        return group;
     }
 
     @PostMapping("/registerGroups")
-    public ResponseEntity<String> registerUser(@RequestParam(defaultValue = "master") String realm, @RequestBody List<User> users) {
-        UserRepresentation user = new UserRepresentation();
-        CredentialRepresentation credential = new CredentialRepresentation();
-        credential.setType(CredentialRepresentation.PASSWORD);
-        return ResponseEntity.ok("User " + " registered");
+    @ResponseBody
+    public SimpleResponse registerUser(@RequestParam(defaultValue = "master") String realm, @RequestBody List<Group> groups) {
+        List<Response> responses = new ArrayList<>();
+        for (Group g : groups) {
+            GroupRepresentation group = generateGroupRep(g);
+
+            Response response = keycloak.realm(realm).groups().add(group);
+            responses.add(response);
+
+            if (response.getStatus() != 201) {
+                logger.error("Error creating group: " + g.getName());
+                return new SimpleResponse(response.getStatus(), response.getStatusInfo().toString());
+            } else {
+                response.close();
+            }
+        }
+        logger.info(responses.size() + " groups registered");
+
+        return new SimpleResponse(HttpStatus.CREATED.value(), responses.size() + " groups registered");
+
+    }
+
+    @ExceptionHandler(java.lang.Exception.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public SimpleResponse handleException(java.lang.Exception e) {
+        return new SimpleResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+//        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
--- generator/src/main/java/com/deceptionkit/registration/UserRegistration.java
+++ generator/src/main/java/com/deceptionkit/registration/UserRegistration.java
@@ -4,12 +4,13 @@ import com.deceptionkit.model.Credential;
 import com.deceptionkit.model.User;
 import com.deceptionkit.response.model.SimpleResponse;
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.ClientResource;
+import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,7 +31,29 @@ public class UserRegistration {
         this.logger = org.slf4j.LoggerFactory.getLogger(UserRegistration.class);
     }
 
-    @PostMapping("/registerUsers")
+    private UserRepresentation generateUserRep(User u) {
+        UserRepresentation user = new UserRepresentation();
+        user.setUsername(u.getUsername());
+        user.setFirstName(u.getFirstName());
+        user.setLastName(u.getLastName());
+        user.setEmail(u.getEmail());
+        user.setEnabled(u.getEnabled());
+        user.setGroups(u.getGroups());
+        List<CredentialRepresentation> tempCred = new ArrayList<>();
+        for (Credential c : u.getCredentials()) {
+            CredentialRepresentation credential = new CredentialRepresentation();
+//            if (c.getType().equals("secret")) {
+//                credential.setType(CredentialRepresentation.SECRET);
+//            }
+            credential.setType(c.getType());
+            credential.setValue(c.getValue());
+            tempCred.add(credential);
+        }
+        user.setCredentials(tempCred);
+        return user;
+    }
+
+    @PostMapping(value = "/registerUsers", consumes = "application/json", produces = "application/json")
     @ResponseBody
     public SimpleResponse registerUser(@RequestParam(defaultValue = "master") String realm, @RequestBody List<User> users) {
         List<Response> responses = new ArrayList<>();
@@ -43,39 +66,20 @@ public class UserRegistration {
             if (response.getStatus() != 201) {
                 logger.error("Failed to register user: " + u.getUsername());
                 return new SimpleResponse(response.getStatus(), "Failed to register user: " + u.getUsername());
-            }
-            else {
+            } else {
                 response.close();
             }
         }
         logger.info(responses.size() + " users registered");
 
-        return new SimpleResponse(201, responses.size() + " users registered");
-    }
-
-    private static UserRepresentation generateUserRep(User u) {
-        UserRepresentation user = new UserRepresentation();
-        user.setUsername(u.getUsername());
-        user.setFirstName(u.getFirstName());
-        user.setLastName(u.getLastName());
-        user.setEmail(u.getEmail());
-        user.setEnabled(u.getEnabled());
-        user.setGroups(u.getGroups());
-        List<CredentialRepresentation> tempCred = new ArrayList<>();
-        for (Credential c : u.getCredentials()) {
-            CredentialRepresentation credential = new CredentialRepresentation();
-            credential.setType(c.getType());
-            credential.setValue(c.getValue());
-            tempCred.add(credential);
-        }
-        user.setCredentials(tempCred);
-        return user;
+        return new SimpleResponse(HttpStatus.CREATED.value(), responses.size() + " users registered");
     }
 
     @ExceptionHandler(java.lang.Exception.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleException(java.lang.Exception e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public SimpleResponse handleException(java.lang.Exception e) {
+        return new SimpleResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+//        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
 }
