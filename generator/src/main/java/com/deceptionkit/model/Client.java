3444e722556ae79fa2ab823f356cdd6d6c13d5a7
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2023-12-03 16:28:42 +0100

    Defined ClientRegistration endpoint

---
+++ generator/src/main/java/com/deceptionkit/model/Client.java
@@ -0,0 +1,152 @@
+package com.deceptionkit.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class Client {
+
+    protected String clientId;
+    protected String name;
+    protected String description;
+    protected String rootUrl;
+    protected String adminUrl;
+    protected String baseUrl;
+    protected Boolean enabled;
+    protected String clientAuthenticatorType;
+    protected List<String> redirectUris;
+    protected List<String> webOrigins;
+    protected Boolean standardFlowEnabled;
+    protected Boolean implicitFlowEnabled;
+    protected Boolean directAccessGrantsEnabled;
+    protected Boolean publicClient;
+    protected String protocol;
+    protected Map<String, String> attributes;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRootUrl() {
+        return rootUrl;
+    }
+
+    public void setRootUrl(String rootUrl) {
+        this.rootUrl = rootUrl;
+    }
+
+    public String getAdminUrl() {
+        return adminUrl;
+    }
+
+    public void setAdminUrl(String adminUrl) {
+        this.adminUrl = adminUrl;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getClientAuthenticatorType() {
+        return clientAuthenticatorType;
+    }
+
+    public void setClientAuthenticatorType(String clientAuthenticatorType) {
+        this.clientAuthenticatorType = clientAuthenticatorType;
+    }
+
+    public List<String> getRedirectUris() {
+        return redirectUris;
+    }
+
+    public void setRedirectUris(List<String> redirectUris) {
+        this.redirectUris = redirectUris;
+    }
+
+    public List<String> getWebOrigins() {
+        return webOrigins;
+    }
+
+    public void setWebOrigins(List<String> webOrigins) {
+        this.webOrigins = webOrigins;
+    }
+
+    public Boolean getStandardFlowEnabled() {
+        return standardFlowEnabled;
+    }
+
+    public void setStandardFlowEnabled(Boolean standardFlowEnabled) {
+        this.standardFlowEnabled = standardFlowEnabled;
+    }
+
+    public Boolean getImplicitFlowEnabled() {
+        return implicitFlowEnabled;
+    }
+
+    public void setImplicitFlowEnabled(Boolean implicitFlowEnabled) {
+        this.implicitFlowEnabled = implicitFlowEnabled;
+    }
+
+    public Boolean getDirectAccessGrantsEnabled() {
+        return directAccessGrantsEnabled;
+    }
+
+    public void setDirectAccessGrantsEnabled(Boolean directAccessGrantsEnabled) {
+        this.directAccessGrantsEnabled = directAccessGrantsEnabled;
+    }
+
+    public Boolean getPublicClient() {
+        return publicClient;
+    }
+
+    public void setPublicClient(Boolean publicClient) {
+        this.publicClient = publicClient;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public Map<String, String> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map<String, String> attributes) {
+        this.attributes = attributes;
+    }
+}
---
+++ generator/src/main/java/com/deceptionkit/registration/ClientRegistration.java
@@ -0,0 +1,59 @@
+package com.deceptionkit.registration;
+
+import com.deceptionkit.model.Client;
+import com.deceptionkit.response.model.SimpleResponse;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ClientRegistration {
+
+    private final Logger logger;
+
+    private final Keycloak keycloak;
+
+    @Autowired
+    public ClientRegistration(Keycloak keycloak) {
+        this.keycloak = keycloak;
+        this.logger = org.slf4j.LoggerFactory.getLogger(ClientRegistration.class);
+    }
+
+    private ClientRepresentation generateClientRep(String clientId) {
+        ClientRepresentation client = new ClientRepresentation();
+        client.setClientId(clientId);
+        return client;
+    }
+
+    @PostMapping("/registerClients")
+    @ResponseBody
+    public SimpleResponse registerClient(@RequestParam(defaultValue = "master") String realm, @RequestBody List<Client> clients) {
+        List<Response> responses = new ArrayList<>();
+        for (Client c : clients) {
+            ClientRepresentation client = generateClientRep(c.getClientId());
+
+            Response response = keycloak.realm(realm).clients().create(client);
+            responses.add(response);
+
+            if (response.getStatus() != 201) {
+                logger.error("Error creating client: " + c.getClientId());
+                return new SimpleResponse(response.getStatus(), response.getStatusInfo().toString());
+            } else {
+                response.close();
+            }
+        }
+        return new SimpleResponse(HttpStatus.CREATED.value(), responses.size() + " clients registered");
+    }
+
+}
