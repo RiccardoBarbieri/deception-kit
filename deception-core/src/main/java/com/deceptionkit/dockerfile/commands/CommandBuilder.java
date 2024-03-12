38bd37f2c4577ab2c8d8ff7c414c380a96414465
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-03-12 01:42:37 +0100

    Completed CommandBuilder base methods, specific methods to be added

--- deception-core/src/main/java/com/deceptionkit/dockerfile/commands/CommandBuilder.java
+++ deception-core/src/main/java/com/deceptionkit/dockerfile/commands/CommandBuilder.java
@@ -6,5 +6,74 @@ public class CommandBuilder {
         return new AddCommand();
     }
 
+    public static ArgCommand arg() {
+        return new ArgCommand();
+    }
+
+    public static CmdCommand cmd() {
+        return new CmdCommand();
+    }
+
+    public static CopyCommand copy() {
+        return new CopyCommand();
+    }
+
+    public static EntrypointCommand entrypoint() {
+        return new EntrypointCommand();
+    }
+
+    public static EnvCommand env() {
+        return new EnvCommand();
+    }
+
+    public static ExposeCommand expose() {
+        return new ExposeCommand();
+    }
+
+    public static FromCommand from() {
+        return new FromCommand();
+    }
+
+    public static HealthcheckCommand healthcheck() {
+        return new HealthcheckCommand();
+    }
+
+    public static LabelCommand label() {
+        return new LabelCommand();
+    }
+
+    @Deprecated
+    public static MaintainerCommand maintainer() {
+        return new MaintainerCommand();
+    }
+
+    public static OnbuildCommand onbuild() {
+        return new OnbuildCommand();
+    }
+
+    public static RunCommand run() {
+        return new RunCommand();
+    }
+
+    public static ShellCommand shell() {
+        return new ShellCommand();
+    }
+
+    public static StopsignalCommand stopsignal() {
+        return new StopsignalCommand();
+    }
+
+    public static UserCommand user() {
+        return new UserCommand();
+    }
+
+    public static VolumeCommand volume() {
+        return new VolumeCommand();
+    }
+
+    public static WorkdirCommand workdir() {
+        return new WorkdirCommand();
+    }
+
 
 }
