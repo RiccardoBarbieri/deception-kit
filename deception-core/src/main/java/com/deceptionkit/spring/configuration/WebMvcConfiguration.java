0f161180fbb2dcac0929ffa7c1eac7cab304dd39
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-03-14 16:30:19 +0100

    Implemented and registered YAML HttpMessageConverter, supports:  - application/yaml  - text/yaml  - application/yml  - text/yml

--- deception-core/src/main/java/com/deceptionkit/spring/configuration/WebMvcConfiguration.java
+++ deception-core/src/main/java/com/deceptionkit/spring/configuration/WebMvcConfiguration.java
@@ -1,20 +1,19 @@
 package com.deceptionkit.spring.configuration;
 
 import com.deceptionkit.spring.apiversion.ApiVersionRequestMappingHandlerMapping;
-import com.deceptionkit.spring.yamlconverter.YamlMessageConverter;
+import com.deceptionkit.spring.yamlconverter.YamlHttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import java.util.List;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
     @Bean
@@ -25,7 +24,7 @@ class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new YamlMessageConverter());
+        converters.add(new YamlHttpMessageConverter<>());
     }
 
 }
