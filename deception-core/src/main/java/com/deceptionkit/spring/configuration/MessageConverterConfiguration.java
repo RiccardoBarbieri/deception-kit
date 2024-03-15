0ac4370aeb81bb40883d295b4b6fba569ba3626d
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-03-15 13:28:54 +0100

    fixed message converter

---
+++ deception-core/src/main/java/com/deceptionkit/spring/configuration/MessageConverterConfiguration.java
@@ -0,0 +1,22 @@
+package com.deceptionkit.spring.configuration;
+
+import com.deceptionkit.spring.yaml.YamlHttpMessageConverter;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+public class MessageConverterConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new YamlHttpMessageConverter<>());
+    }
+}
