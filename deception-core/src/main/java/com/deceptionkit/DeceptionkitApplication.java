666dfd89c9a15cbfb55d3c2d99123ba15eaa319f
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-03-30 12:58:43 +0100

    application startup broke down to modify parameters

--- deception-core/src/main/java/com/deceptionkit/DeceptionkitApplication.java
+++ deception-core/src/main/java/com/deceptionkit/DeceptionkitApplication.java
@@ -1,13 +1,17 @@
 package com.deceptionkit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
 
 @SpringBootApplication
 public class DeceptionkitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeceptionkitApplication.class, args);
+		SpringApplication springApp = new SpringApplication(DeceptionkitApplication.class);
+		springApp.run(args);
 	}
 
 }
