e3224b67b432cdbc659e0962dfb145ccad643d46
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-06-05 15:12:56 +0200

    Configured global exception handling with message response

--- deception-core/src/main/java/com/deceptionkit/spring/configuration/RequestMappingConfiguration.java
+++ deception-core/src/main/java/com/deceptionkit/spring/configuration/RequestMappingConfiguration.java
@@ -1,12 +1,16 @@
 package com.deceptionkit.spring.configuration;
 
 import com.deceptionkit.spring.apiversion.ApiVersionRequestMappingHandlerMapping;
+import com.deceptionkit.spring.errorhandler.RestResponseStatusExceptionResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -16,5 +20,10 @@ class RequestMappingConfiguration implements WebMvcConfigurer {
     @Primary
     public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
         return new ApiVersionRequestMappingHandlerMapping("v");
+    }
+
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+        exceptionResolvers.add(0, new RestResponseStatusExceptionResolver());
     }
 }
