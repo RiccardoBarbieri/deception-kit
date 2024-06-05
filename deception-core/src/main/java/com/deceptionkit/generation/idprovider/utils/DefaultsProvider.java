a94eb348a85a3cac62ed7d4a91706d03fcf5cfaf
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-06-05 09:30:01 +0200

    Fixed resource retrieval in DefaultsProvider

--- deception-core/src/main/java/com/deceptionkit/generation/idprovider/utils/DefaultsProvider.java
+++ deception-core/src/main/java/com/deceptionkit/generation/idprovider/utils/DefaultsProvider.java
@@ -2,8 +2,11 @@ package com.deceptionkit.generation.idprovider.utils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,16 +15,24 @@ import java.io.InputStream;
 @Component
 public class DefaultsProvider {
 
-    @Value("${client.default.file}")
+    private static final Logger log = LoggerFactory.getLogger(DefaultsProvider.class);
+
+
     private static String DEFAULTS_FILE;
 
+    @Value("${client.default.file}")
+    public void setDEFAULTS_FILE(String DEFAULTS_FILE) {
+        this.DEFAULTS_FILE = DEFAULTS_FILE;
+    }
 
-    private DefaultsProvider() {
+
+    public DefaultsProvider() {
 
     }
 
     public static <T> T getClientDefault(String key, Class<T> type) {
-        try (InputStream is = new FileInputStream(DEFAULTS_FILE)) {
+        log.debug("DEFAULTS_FILE: {}", DEFAULTS_FILE);
+        try (InputStream is = new FileInputStream(ResourceUtils.getFile("classpath:" + DEFAULTS_FILE))) {
             JsonNode node = (new ObjectMapper()).readTree(is);
             return (new ObjectMapper()).treeToValue(node.get(key), type);
         } catch (IOException e) {
