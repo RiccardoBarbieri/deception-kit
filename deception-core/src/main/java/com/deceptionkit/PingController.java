5454e541493a4b6765ebf29731114ab65dd81a5c
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-06-05 19:53:10 +0200

    Added responsebody to PingController

--- deception-core/src/main/java/com/deceptionkit/PingController.java
+++ deception-core/src/main/java/com/deceptionkit/PingController.java
@@ -2,11 +2,13 @@ package com.deceptionkit;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PingController {
 
     @GetMapping("/ping")
+    @ResponseBody
     public String ping() {
         return "pong";
     }
