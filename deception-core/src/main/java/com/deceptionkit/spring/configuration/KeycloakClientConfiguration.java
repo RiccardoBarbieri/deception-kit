37d1d38ac0b949b3018073b61ec0f68a6eeaff98
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-06-11 14:44:25 +0200

    Parametrized HOST and PORT for keycloak client

--- deception-core/src/main/java/com/deceptionkit/spring/configuration/KeycloakClientConfiguration.java
+++ deception-core/src/main/java/com/deceptionkit/spring/configuration/KeycloakClientConfiguration.java
@@ -3,16 +3,33 @@ package com.deceptionkit.spring.configuration;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class KeycloakClientConfiguration {
 
+    private String HOST;
+
+    private String PORT;
+
+    @Value("${keycloak.host}")
+    public void setHOST(String HOST) {
+        this.HOST = HOST;
+    }
+
+    @Value("${keycloak.port}")
+    public void setPORT(String PORT) {
+        this.PORT = PORT;
+    }
+
+
     @Bean
     public Keycloak keycloak() {
         return KeycloakBuilder.builder()
-                .serverUrl("http://localhost:8100")
+                .serverUrl("http://" + HOST + ":" + PORT)
                 .grantType(OAuth2Constants.PASSWORD)
                 .realm("master")
                 .clientId("admin-cli")
